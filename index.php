04e978f30a2af1484ff4aecf4602beaccb72964c
popovichppravi <popovichppravi@gmail.com>
2015-04-14 06:37:48 +0300

    50% header

--- index.php
+++ index.php
@@ -1,1 +1,79 @@
-тест
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	 <link href="http://allfont.ru/css/?fonts=roboto-light" rel="stylesheet" type="text/css" />
+	<link href="template_styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="top_rectangle"></div>	
+<div class="wrapper">
+
+
+	<header class="header">
+
+	<div id="top_header">
+	<!-- // LOGO -->
+	<div id="logo">  
+          <a href="#" title="">
+          <img src="images/logo.png" alt="">        
+        </a>
+    </div>
+	<!-- LOGO // -->
+
+	<!-- // Top Name -->
+	<div id="top_name">
+	<p>Официальный сайт</p>
+	<span>
+		Алтайская академия<br>
+		экономики и права
+	</span>		
+	</div>
+	<!--  Top Name // -->
+
+	<!-- // Top Phone  -->
+	<div id="top_phone">
+             <div class="tp_phone">(385) 224-48-08</div>     
+             <div class="zvonok"><a href="#">Обратная связь</a></div>
+     </div>
+	<!--  Top Phone //  -->
+
+	<!-- // Top Search  -->
+	<div id="top_search">
+      <form>
+          <div class="search">
+             <input type="text" name="login" placeholder="Поиск ...">
+             <input class="btn_blue" name="s" type="submit" value="Найти">
+           </div>
+      </form>
+      <div class="top_map"><a href="#">Карта сайта</a></div>
+      <div class="top_rss"><a href="#">RSS подписка</a></div>
+    </div>
+	<!--  Top Search  //-->
+
+</div>	
+<div id="bottom_header">
+<div class="menu_btn">меню</div>
+</div>
+
+
+	</header><!-- .header-->
+
+	<main class="content">
+		<strong>Content:</strong> Sed placerat accumsan ligula. Aliquam felis magna, congue quis, tempus eu, aliquam vitae, ante. Cras neque justo, ultrices at, rhoncus a, facilisis eget, nisl. Quisque vitae pede. Nam et augue. Sed a elit. Ut vel massa. Suspendisse nibh pede, ultrices vitae, ultrices nec, mollis non, nibh. In sit amet pede quis leo vulputate hendrerit. Cras laoreet leo et justo auctor condimentum. Integer id enim. Suspendisse egestas, dui ac egestas mollis, libero orci hendrerit lacus, et malesuada lorem neque ac libero. Morbi tempor pulvinar pede. Donec vel elit.
+	</main><!-- .content -->
+
+</div><!-- .wrapper -->
+
+<footer class="footer">
+	<strong>Footer:</strong> Mus elit Morbi mus enim lacus at quis Nam eget morbi. Et semper urna urna non at cursus dolor vestibulum neque enim. Tellus interdum at laoreet laoreet lacinia lacinia sed Quisque justo quis. Hendrerit scelerisque lorem elit orci tempor tincidunt enim Phasellus dignissim tincidunt. Nunc vel et Sed nisl Vestibulum odio montes Aliquam volutpat pellentesque. Ut pede sagittis et quis nunc gravida porttitor ligula.
+</footer><!-- .footer -->
+
+</body>
+</html>
