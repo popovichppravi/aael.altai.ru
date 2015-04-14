3769c060f47e094c263e501044d4663c19b6df0d
popovichppravi <popovichppravi@gmail.com>
2015-04-14 16:26:59 +0300

    main - 40%

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 	<meta name="description" content="" />
 	 <link href="http://allfont.ru/css/?fonts=roboto-light" rel="stylesheet" type="text/css" />
 	<link href="template_styles.css" rel="stylesheet">
+
+	<!--Main Slider-->
+	<link rel="stylesheet" type="text/css" href="slider/basic.css"/>
+<link rel="stylesheet" type="text/css" href="slider/theme.css"/>
+<link rel="stylesheet" type="text/css" href="carousel/assets/owl.carousel.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="slider/js/JwgSlider.min.js"></script>
+<script type="text/javascript" src="carousel/owl.carousel.min.js"></script>
+<script type="text/javascript" src="carousel/owl.carousel.js"></script>
 </head>
 
 <body>
@@ -61,7 +70,7 @@
 <div id="bottom_header">
 	<div class="menu_btn"><a href="#">меню</a></div>
 
-
+<!-- // Top Menu -->
 <div id="menu_top">
 <div>
 <ul>
@@ -76,6 +85,7 @@
 </ul>
 </div>
 </div>
+<!--  Top Menu  //-->
 
 </div>
 
@@ -83,7 +93,225 @@
 	</header><!-- .header-->
 
 	<main class="content">
-		<strong>Content:</strong> Sed placerat accumsan ligula. Aliquam felis magna, congue quis, tempus eu, aliquam vitae, ante. Cras neque justo, ultrices at, rhoncus a, facilisis eget, nisl. Quisque vitae pede. Nam et augue. Sed a elit. Ut vel massa. Suspendisse nibh pede, ultrices vitae, ultrices nec, mollis non, nibh. In sit amet pede quis leo vulputate hendrerit. Cras laoreet leo et justo auctor condimentum. Integer id enim. Suspendisse egestas, dui ac egestas mollis, libero orci hendrerit lacus, et malesuada lorem neque ac libero. Morbi tempor pulvinar pede. Donec vel elit.
+	<!--  Main Slider  //-->
+
+  <div class="jwg_slider_module" style="height:290px">
+    <div class="arrow_navigation">
+      <div class="left">
+
+        <p> <img src="slider/img/arr1.png" height="27" width="15"></p>
+      </div>
+      <div class="right">
+        <p> <img src="slider/img/arr2.png" height="27" width="15"></p>
+      </div>
+    </div>
+    <div class="slides">
+      <div class="slide">
+      <img src="images/slide.png" width="520" height="290" class="big_img">
+        <div class="block_right">
+        	<span class="medium">Открытые двери для Всех желающих каждую субботу в 17.00 в актовом зале</span>
+
+        	<span class="light">
+        		Ждем наших абитуриентов<br> в стенах Академии<br>с 10 июня по 30 июня 2015 г.
+        	</span>
+
+        </div>
+      </div>
+     
+      <div class="slide">
+      <img src="images/slide.png" width="520" height="290" class="big_img">
+        <div class="block_right">
+        	<span class="medium">Открытые двери для Всех желающих каждую субботу в 17.00 в актовом зале</span>
+
+        	<span class="light">
+        		Ждем наших абитуриентов<br> в стенах Академии<br>с 10 июня по 30 июня 2015 г.
+        	</span>
+
+        </div>
+      </div>
+
+       <div class="slide">
+      <img src="images/slide.png" width="520" height="290" class="big_img">
+        <div class="block_right">
+        	<span class="medium">Открытые двери для Всех желающих каждую субботу в 17.00 в актовом зале</span>
+
+        	<span class="light">
+        		Ждем наших абитуриентов<br> в стенах Академии<br>с 10 июня по 30 июня 2015 г.
+        	</span>
+
+        </div>
+      </div>
+    </div>
+    <div class="tabbed_navigation">
+      <ul>
+        <li>
+        <div></div>
+        </li>
+        <li>
+        <div></div>
+        </li>
+        <li>
+        <div></div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <script>
+		  $('.jwg_slider_module').jwgSlider('both', 400); 
+	</script>
+	<!--  Main Slider  //-->
+
+<!-- // Main News -->
+<div id="main_news">
+<div class="top_news_block">
+<div class="left_news_block">
+	<span class="h1_main_block news">Новости</span>
+</div>
+<div class="right_news_block">
+<a href="#">Все новости</a>
+	<span class="h1_main_block anonses">Анонсы</span>
+	<a href="#">Все анонсы</a>
+</div>
+</div>
+
+<div class="bottom_news_block">
+	<ul>
+		<li>
+		<p class="date">
+			2 февраля 2015г.
+		</p>
+
+		<p class="h2_news">
+			С 2 февраля поднимется стоимость проезда в городском общественном транспорте
+		</p>
+		<p class="txt">
+			Мэрия города вводит новые тарифы на проезд в городском наземном транспорте города 
+Барнаул.
+		</p>
+		<a href="#">Подробнее</a>
+
+		</li>
+
+		<li>
+		<p class="date">
+			2 февраля 2015г.
+		</p>
+
+		<p class="h2_news">
+			С 2 февраля поднимется стоимость проезда в городском общественном транспорте
+		</p>
+		<p class="txt">
+			Мэрия города вводит новые тарифы на проезд в городском наземном транспорте города 
+Барнаул.
+		</p>
+		<a href="#">Подробнее</a>
+
+		</li>
+
+		<li>
+		<p class="date">
+			2 февраля 2015г.
+		</p>
+
+		<p class="h2_news">
+			С 2 февраля поднимется стоимость проезда в городском общественном транспорте
+		</p>
+		<p class="txt">
+			Мэрия города вводит новые тарифы на проезд в городском наземном транспорте города 
+Барнаул.
+		</p>
+		<a href="#">Подробнее</a>
+
+		</li>
+
+		<li>
+		<p class="date">
+			2 февраля 2015г.
+		</p>
+
+		<p class="h2_news">
+			С 2 февраля поднимется стоимость проезда в городском общественном транспорте
+		</p>
+		<p class="txt">
+			Мэрия города вводит новые тарифы на проезд в городском наземном транспорте города 
+Барнаул.
+		</p>
+		<a href="#">Подробнее</a>
+
+		</li>
+	</ul>
+</div>
+	
+</div>
+<div class="shape"></div>
+<!--  Main News  //-->
+
+<!-- // Main Photo -->
+<div id="main_photo">
+<div class="top_photo_block">
+<div class="left_photo_block">
+	<span class="h1_main_block photo">Фотогалерея</span>
+</div>
+<div class="right_photo_block">
+<a href="#">Все фотографии</a>
+</div>
+</div>
+
+<div class="bottom_photo_block">
+<div class="owl-carousel">
+    <div class="item"><img src="images/foto1.png" height="210" width="210"></div>
+    <div class="item"><img src="images/foto2.png" height="210" width="210"></div>
+    <div class="item"><img src="images/foto3.png" height="210" width="210"></div>
+    <div class="item"><img src="images/foto4.png" height="210" width="210"></div>
+</div>
+<script>	
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+</script>
+</div>
+
+<div class="shape"></div>
+<!-- Main Photo-->
+
+<div id="main_about">
+<div class="top_about_block">
+<div class="left_about_block">
+	<span class="h1_main_block about">Об академии</span>
+</div>
+</div>
+
+<div class="bottom_about_block">
+<div id="middle_menu">
+<ul>
+	<li><a href="#"><img src="images/mm1.png"><br><b>Ректор</b></a></li>
+	<li><a href="#"><img src="images/mm2.png"><br>Юридический факультет</a></li>
+	<li><a href="#"><img src="images/mm3.png"><br>Факультет ЭУВД</a></li>
+	<li><a href="#"><img src="images/mm4.png"><br>Заочное отделение</a></li>
+	<li><a href="#"><img src="images/mm5.png"><br>Центры дополнительного образования</a></li>
+	<li><a href="#"><img src="images/mm6.png"><br>Научные и экспертные центры</a></li>
+</ul>
+	
+</div>
+</div>
+
+</div>
+<!-- //MAin About -->
+
+<!-- Main About //-->
 	</main><!-- .content -->
 
 </div><!-- .wrapper -->
